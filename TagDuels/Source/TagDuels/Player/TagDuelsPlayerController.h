08dbd8374f9faa7fcbfb305e89e419afb20a9b30
Dmitriy <dimka_dmit@mail.ru>
2025-12-22 16:56:25 +0300

    still fixing interaction

---
+++ TagDuels/Source/TagDuels/Player/TagDuelsPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TagDuelsPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAGDUELS_API ATagDuelsPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
