517023514c5c30d7ee1c163e2f26caf59a8ad1bb
TimurTimergalin <timur20041229@gmail.com>
2026-03-12 23:04:27 +0300

    permissions

--- fleet_manager/run_server.sh
+++ fleet_manager/run_server.sh
@@ -2,5 +2,8 @@
 
 docker build -f ServerDev.Dockerfile -t fleet-manager-server:latest .
 minikube image load fleet-manager-server:latest
+kubectl apply -f fleet-manager-serviceaccount.yaml
+kubectl apply -f fleet-manager-role.yaml
+kubectl apply -f fleet-manager-rolebinding-sa.yaml
 kubectl apply -f fleet-manager-pod.yaml
 kubectl apply -f fleet-manager-service.yaml
