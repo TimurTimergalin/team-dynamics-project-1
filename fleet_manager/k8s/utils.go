76f196135831cdc0afafc2b5ad466ef87d73b1ed
TimurTimergalin <timur20041229@gmail.com>
2026-03-12 23:21:55 +0300

    fix ports

--- fleet_manager/k8s/utils.go
+++ fleet_manager/k8s/utils.go
@@ -3,14 +3,12 @@ package k8s
 import (
 	v1 "agones.dev/agones/pkg/apis/agones/v1"
 	"fmt"
-	"log"
 )
 
 func GetAddress(host string, ports []v1.GameServerStatusPort) *string {
-	log.Printf("Trying to get address, host: %s, ports: %v", host, ports)
 	for _, port := range ports {
 		switch port.Name {
-		case "game":
+		case "default":
 			res := fmt.Sprintf("%s:%d", host, port.Port)
 			return &res
 		}
