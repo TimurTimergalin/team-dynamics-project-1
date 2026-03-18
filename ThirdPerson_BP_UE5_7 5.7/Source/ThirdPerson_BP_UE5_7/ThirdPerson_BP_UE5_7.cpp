8c656d62f288aa86c798a37000078a041c38b25c
TimurTimergalin <timur20041229@gmail.com>
2026-03-18 11:56:04 +0300

    all

---
+++ ThirdPerson_BP_UE5_7 5.7/Source/ThirdPerson_BP_UE5_7/ThirdPerson_BP_UE5_7.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ThirdPerson_BP_UE5_7.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThirdPerson_BP_UE5_7, "ThirdPerson_BP_UE5_7" );
