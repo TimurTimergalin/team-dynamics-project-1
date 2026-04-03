efce2819c8c959e9f71b75e506ddbce51b27c80f
TimurTimergalin <timur20041229@gmail.com>
2026-04-03 23:27:23 +0300

    add logs

--- lib/pg_lib/src/pg_lib/query.go
+++ lib/pg_lib/src/pg_lib/query.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"logging"
 )
 
 type RetryPolicy = int32
@@ -16,27 +17,35 @@ const (
 )
 
 func withTransaction[T any](ctx context.Context, pool *pgxpool.Pool, cfg *QueryConfig, op func(context.Context, *pgxpool.Pool) (_res *T, _err error, _shouldRetry RetryPolicy)) (_res *T, _err *PgLibError, _shouldRetry RetryPolicy) {
+	logger := logging.GetLogger(ctx)
 	tx, err := pool.BeginTx(ctx, pgx.TxOptions{
 		IsoLevel:   cfg.IsolationLevel,
 		AccessMode: cfg.AccessMode,
 	})
 	if err != nil {
+		logger.Debug("Could not begin tx")
 		return nil, makeConnectionError(err), NormalRetry
 	}
 	defer func(tx pgx.Tx, ctx context.Context) {
-		_ = tx.Rollback(ctx)
+		err := tx.Rollback(ctx)
+		if err != nil {
+			logger.Debug("Could not rollback tx", "error", err)
+		}
 	}(tx, ctx)
 
 	res, err, shouldRetry := op(ctx, pool)
 	if err != nil {
 		if shouldRetry == NoRetry {
+			logger.Debug("Some non-retryable error occurred", "error", err)
 			return nil, makeLogicError(err), shouldRetry
 		}
+		logger.Debug("Some retryable error occurred", "error", err)
 		return nil, makeConnectionError(err), shouldRetry
 	}
 
 	err = tx.Commit(ctx)
 	if err != nil {
+		logger.Debug("Could not commit", "error", err)
 		return nil, makeConnectionError(err), NormalRetry
 	}
 
@@ -49,7 +58,8 @@ func withTimeout[T any](ctx context.Context, cfg *QueryConfig, op func(context.C
 	return op(ctx)
 }
 
-func withRetry[T any](cfg *QueryConfig, op func() (_res *T, _err *PgLibError, _shouldRetry RetryPolicy)) (*T, *PgLibError) {
+func withRetry[T any](ctx context.Context, cfg *QueryConfig, op func() (_res *T, _err *PgLibError, _shouldRetry RetryPolicy)) (*T, *PgLibError) {
+	logger := logging.GetLogger(ctx)
 	var lastErr *PgLibError = nil
 	maxAllowedFree := 5
 	for i := cfg.Retries; i > 0; i -= 1 {
@@ -69,11 +79,12 @@ func withRetry[T any](cfg *QueryConfig, op func() (_res *T, _err *PgLibError, _s
 	if lastErr == nil {
 		return nil, makeServerError(fmt.Errorf("no operation was performed"))
 	}
+	logger.Debug("All retries failed", "error", lastErr)
 	return nil, makeConnectionError(lastErr)
 }
 
 func PerformOperation[T any](ctx context.Context, pool *pgxpool.Pool, cfg *QueryConfig, op func(context.Context, *pgxpool.Pool) (*T, error, RetryPolicy)) (*T, *PgLibError) {
-	res, err := withRetry[T](cfg, func() (*T, *PgLibError, RetryPolicy) {
+	res, err := withRetry[T](ctx, cfg, func() (*T, *PgLibError, RetryPolicy) {
 		return withTimeout[T](ctx, cfg, func(timeoutCtx context.Context) (*T, *PgLibError, RetryPolicy) {
 			return withTransaction[T](timeoutCtx, pool, cfg, op)
 		})
