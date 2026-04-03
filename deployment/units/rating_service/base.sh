1996d1943d87325bf0120238a45e9de1a3d9a699
TimurTimergalin <timur20041229@gmail.com>
2026-04-03 19:09:21 +0300

    fix pod

--- deployment/units/rating_service/base.sh
+++ deployment/units/rating_service/base.sh
@@ -1,5 +1,6 @@
 cd $ROOT/deployment/units/rating_service
 docker build --build-arg PROJECT_DIR=services/rating_service -t rating-service-server:latest -f Dockerfile $ROOT
+minikube image load rating-service-server:latest
 kubectl apply -f env.yaml
 kubectl apply -f pod.yaml
 kubectl apply -f service.yaml
