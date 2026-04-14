a6bea2379800856c5436336e4b3a180ddb6a9213
TimurTimergalin <timur20041229@gmail.com>
2026-04-14 20:14:11 +0300

    fix service

--- deployment/units/mm_exec/dev.sh
+++ deployment/units/mm_exec/dev.sh
@@ -1,5 +1,5 @@
 cd $ROOT/deployment/units/mm_exec
-docker build --build-arg PROJECT_DIR=services/match_history_service -t mm-exec:latest -f Dockerfile $ROOT
+docker build --build-arg PROJECT_DIR=services/mm_exec -t mm-exec:latest -f Dockerfile $ROOT
 minikube image load  mm-exec:latest
 kubectl apply -f env.yaml
 kubectl apply -f pod.yaml
