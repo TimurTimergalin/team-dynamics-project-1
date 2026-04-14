7d2e8050ebb538b89f19592fad46d832634502fc
TimurTimergalin <timur20041229@gmail.com>
2026-04-14 14:20:47 +0300

    update sh for secrets

--- deployment/secrets/dev.sh
+++ deployment/secrets/dev.sh
@@ -1,3 +1,6 @@
 source base.sh
 kubectl apply -f dev_rating_storage_credentials_secret.yaml
 kubectl apply -f dev_match_history_storage_credentials_secret.yaml
+kubectl apply -f dev_match_kv_credentials_secret.yaml
+kubectl apply -f dev_mm_pool_credentials.yaml
+kubectl apply -f dev_user_storage_credentials.yaml
