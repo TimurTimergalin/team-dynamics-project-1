c2b0061f0838ad07ba281aacdc4b40a317bcd693
TimurTimergalin <timur20041229@gmail.com>
2026-04-14 19:34:25 +0300

    fix service

--- deployment/units/match_service/dev.sh
+++ deployment/units/match_service/dev.sh
@@ -1,5 +1,5 @@
 cd $ROOT/deployment/units/match_service
-docker build --build-arg PROJECT_DIR=services/match_history_service -t match-service-server:latest -f Dockerfile $ROOT
+docker build --build-arg PROJECT_DIR=services/match_service -t match-service-server:latest -f Dockerfile $ROOT
 minikube image load match-service-server:latest
 kubectl apply -f env.yaml
 kubectl apply -f pod.yaml
