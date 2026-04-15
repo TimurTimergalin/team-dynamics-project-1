24157d33140cfcdd7fb886d53bf1044c6ef1466e
TimurTimergalin <timur20041229@gmail.com>
2026-04-15 20:20:01 +0300

    logs

--- services/mm_exec/scheduler.py
+++ services/mm_exec/scheduler.py
@@ -15,7 +15,7 @@ class Scheduler:
             try:
                 self.job()
             except Exception:
-                print("Job failed:", traceback.format_exc(), file=sys.stderr, sep='\n')
+                print(f"Job failed at: {datetime.datetime.now()}", traceback.format_exc(), file=sys.stderr, sep='\n')
             else:
                 print("Job executed at:", datetime.datetime.now(datetime.timezone.utc))
             end = time.time_ns() // 1_000_000
