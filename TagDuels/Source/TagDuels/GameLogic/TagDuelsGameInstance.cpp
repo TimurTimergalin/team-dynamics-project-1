7132205a327054082dce7863c784e23226de654b
Dmitriy <dimka_dmit@mail.ru>
2026-04-19 19:09:44 +0300

    conflicts resolved

--- TagDuels/Source/TagDuels/GameLogic/TagDuelsGameInstance.cpp
+++ TagDuels/Source/TagDuels/GameLogic/TagDuelsGameInstance.cpp
@@ -6,6 +6,17 @@
 #include "Interfaces/OnlineIdentityInterface.h"
 #include "OnlineSubsystemUtils.h"
 
+// Base Methods
+void UTagDuelsGameInstance::OnStart()
+{
+	Super::OnStart();
+	BlStart();
+}
+void UTagDuelsGameInstance::BlStart_Implementation()
+{
+}
+
+// Steam OSS
 FString UTagDuelsGameInstance::GetSteamAuthToken()
 {
 	// 1. Get the Subsystem (it will auto-detect Steam if configured in .ini)
@@ -48,3 +59,102 @@ FString UTagDuelsGameInstance::GetSteamID()
 	}
 	return FString(TEXT("InvalidID"));
 }
+
+// EpicGames OSS
+void UTagDuelsGameInstance::LoginToEOS()
+{
+    IOnlineSubsystem* OnlineSubsystem = Online::GetSubsystem(GetWorld());
+    if (!OnlineSubsystem)
+    {
+        UE_LOG(LogTemp, Error, TEXT("OnlineSubsystem not available"));
+        return;
+    }
+
+    IOnlineIdentityPtr IdentityInterface = OnlineSubsystem->GetIdentityInterface();
+    if (!IdentityInterface.IsValid())
+    {
+        UE_LOG(LogTemp, Error, TEXT("Identity interface not available"));
+        return;
+    }
+	
+	UE_LOG(LogTemp, Display, TEXT("AccountPortal Start"));
+	// AccountPortal automatically launches the browser for user login
+	FOnlineAccountCredentials Credentials;
+	Credentials.Type = FString("accountportal");
+	Credentials.Id = FString();
+	Credentials.Token = FString();
+    
+    IdentityInterface->AddOnLoginCompleteDelegate_Handle(0, 
+        FOnLoginCompleteDelegate::CreateUObject(this, &UTagDuelsGameInstance::OnLoginComplete));
+	UE_LOG(LogTemp, Display, TEXT("Login"));
+    IdentityInterface->Login(0, Credentials);
+}
+
+void UTagDuelsGameInstance::OnLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error)
+{
+	if (bWasSuccessful)
+	{
+		// Silent login succeeded!
+		UE_LOG(LogTemp, Display, TEXT("Persistent Auth successful for Epic Account ID: %s"), *UserId.ToString());
+
+		// Retrieve Auth Token and send to backend
+		IOnlineSubsystem* OnlineSubsystem = Online::GetSubsystem(GetWorld());
+		IOnlineIdentityPtr IdentityInterface = OnlineSubsystem->GetIdentityInterface();
+
+		FString EpicAccountId = UserId.ToString();
+		FString AuthToken = IdentityInterface->GetAuthToken(LocalUserNum);
+
+		UE_LOG(LogTemp, Display, TEXT("Ready to send to backend - Epic ID: %s, Token: %s"), *EpicAccountId, *AuthToken);
+		// In Game Print String
+		if (GEngine)
+		{
+			FString DebugMessage = FString::Printf(TEXT("Ready to send to backend - Epic ID: %s, Token: %s"), *EpicAccountId, *AuthToken);
+    
+			GEngine->AddOnScreenDebugMessage(
+				-1,                      // Unique key (-1 means it won't overwrite previous messages)
+				10.0f,                   // Duration in seconds
+				FColor::Green,           // Text color
+				DebugMessage             // The actual string
+			);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Persistent Auth failed. Error: %s"), *Error);
+	}
+}
+
+// Online
+EOnlineSubsystemType UTagDuelsGameInstance::GetActiveOnlineSubsystemType() const
+{
+	FName SubsystemName = GetOnlinePlatformName(); // "Steam", "EOS", "NULL" и т.д.
+
+	// IOnlineSubsystem* EpicOSS = IOnlineSubsystem::Get(TEXT("EOS"));
+	//
+	// IOnlineSubsystem* SteamOSS = IOnlineSubsystem::Get(TEXT("Steam"));
+
+	if (SubsystemName == FName(TEXT("Steam")))
+	{
+		return EOnlineSubsystemType::Steam;
+	}
+	if (SubsystemName == FName(TEXT("EOS")))
+	{
+		return EOnlineSubsystemType::EOS;
+	}
+	if (SubsystemName == FName(TEXT("NULL")))
+	{
+		return EOnlineSubsystemType::Null;
+	}
+	if (SubsystemName == NAME_None)
+	{
+		return EOnlineSubsystemType::None;
+	}
+    
+	// На случай неизвестных подсистем
+	UE_LOG(LogTemp, Warning, TEXT("Unknown online subsystem: %s"), *SubsystemName.ToString());
+	return EOnlineSubsystemType::None;
+}
+
+
+
+
--- TagDuels/Source/TagDuels/GameLogic/TagDuelsGameInstance.h
+++ TagDuels/Source/TagDuels/GameLogic/TagDuelsGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "TagDuels/Enums/TagDuelsEnums.h"
 #include "TagDuelsGameInstance.generated.h"
 
 /**
@@ -15,9 +16,28 @@ class TAGDUELS_API UTagDuelsGameInstance : public UGameInstance
 	GENERATED_BODY()
 
 public:
+	// Base Methods
+	virtual void OnStart() override;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "PlayerController")
+	void BlStart();
+	
+	// Steam OSS
 	UFUNCTION(BlueprintCallable, Category = "SteamAuth")
 	FString GetSteamAuthToken();
 
 	UFUNCTION(BlueprintCallable, Category = "SteamAuth")
 	FString GetSteamID();
+
+	// EpicGames OSS
+	UFUNCTION(BlueprintCallable, Category = "EGSAuth")
+	void LoginToEOS();
+
+	// Online
+	UFUNCTION(BlueprintCallable, Category = "Online")
+	EOnlineSubsystemType GetActiveOnlineSubsystemType() const;
+
+private:
+	// EpicGames OSS
+	void OnLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
 };
