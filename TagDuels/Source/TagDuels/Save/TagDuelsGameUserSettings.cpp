643d81d941a78fc6571dce09431cdcada16299c8
Dmitriy <dimka_dmit@mail.ru>
2026-04-21 17:17:22 +0300

    imported menu plugin

---
+++ TagDuels/Source/TagDuels/Save/TagDuelsGameUserSettings.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TagDuelsGameUserSettings.h"
+
+float UTagDuelsGameUserSettings::GetMasterVolume()
+{
+	return MasterVolume;
+}
+
+void UTagDuelsGameUserSettings::SetMasterVolume(float Volume)
+{
+	MasterVolume = Volume;
+}
+
+float UTagDuelsGameUserSettings::GetMusicVolume()
+{
+	return MusicVolume;
+}
+
+void UTagDuelsGameUserSettings::SetMusicVolume(float Volume)
+{
+	MusicVolume = Volume;
+}
+
+float UTagDuelsGameUserSettings::GetSFXVolume()
+{
+	return SFXVolume;
+}
+
+void UTagDuelsGameUserSettings::SetSFXVolume(float Volume)
+{
+	SFXVolume = Volume;
+}
---
+++ TagDuels/Source/TagDuels/Save/TagDuelsGameUserSettings.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "TagDuelsGameUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAGDUELS_API UTagDuelsGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category=Settings)
+	float GetMasterVolume();
+
+	UFUNCTION(BlueprintCallable, Category=Settings)
+	void SetMasterVolume(float Volume);
+
+	UFUNCTION(BlueprintPure, Category=Settings)
+	float GetMusicVolume();
+
+	UFUNCTION(BlueprintCallable, Category=Settings)
+	void SetMusicVolume(float Volume);
+
+	UFUNCTION(BlueprintPure, Category=Settings)
+	float GetSFXVolume();
+
+	UFUNCTION(BlueprintCallable, Category=Settings)
+	void SetSFXVolume(float Volume);
+	
+private:
+	UPROPERTY(config)
+	float MasterVolume = 1.0f;
+	
+	UPROPERTY(config)
+	float MusicVolume = 1.0f;
+
+	UPROPERTY(config)
+	float SFXVolume = 1.0f;
+};
