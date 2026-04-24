6f57fba14a10b4372325c92d71d86d2320be7238
TimurTimergalin <timur20041229@gmail.com>
2026-04-24 23:10:09 +0300

    user_kv_repo

--- services/user_event/redis/keys.go
+++ services/user_event/redis/keys.go
@@ -3,25 +3,29 @@ package redis
 import "fmt"
 
 type PlayerKeySet struct {
-	id int64
+	Id int64
 }
 
 func (k PlayerKeySet) name() string {
-	return fmt.Sprintf("name:%d", k.id)
+	return fmt.Sprintf("name:%d", k.Id)
 }
 
 func (k PlayerKeySet) rating() string {
-	return fmt.Sprintf("rating:%d", k.id)
+	return fmt.Sprintf("rating:%d", k.Id)
 }
 
 func (k PlayerKeySet) subscriptions() string {
-	return fmt.Sprintf("subscriptions:%d", k.id)
+	return fmt.Sprintf("subscriptions:%d", k.Id)
 }
 
 func (k PlayerKeySet) currentChallenge() string {
-	return fmt.Sprintf("currentChallenge:%d", k.id)
+	return fmt.Sprintf("currentChallenge:%d", k.Id)
 }
 
 func (k PlayerKeySet) status() string {
-	return fmt.Sprintf("status:%d", k.id)
+	return fmt.Sprintf("status:%d", k.Id)
+}
+
+func (k PlayerKeySet) keys() []string {
+	return []string{k.name(), k.rating(), k.subscriptions(), k.currentChallenge(), k.status()}
 }
